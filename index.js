3b8171625a6f44fa0b28c5b227ecb77ea36b570c
Joel Mwaga <mwagahjoel14@gmail.com>
2025-02-27 08:28:38 +0300

    changed 1 file

---
+++ index.js
@@ -0,0 +1,31 @@
+add();
+function add(a, b){
+  return a + b;
+
+}
+subtract();
+function subtract(a, b){
+  return a - b;
+
+}
+multiply();
+function multiply(a, b){
+  return a * b;
+
+}
+divide();
+function divide(a, b){
+  return a / b;
+} 
+function increment(n){
+  return n += 1;
+}
+function decrement(n){
+  return n -= 1;
+}
+function makeInt(n){
+  return parseInt(n, 10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
